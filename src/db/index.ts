08a0c98ef7b02341f3d11821b751e8370e673799
Tomal Hossen <tomalhossen78@gmail.com>
2026-05-16 14:31:02 +0600

    clean up server with app.ts

---
+++ src/db/index.ts
@@ -0,0 +1,28 @@
+import { Pool } from "pg";
+import config from "../config";
+
+export const pool = new Pool({
+  connectionString: config.conection_string,
+});
+
+export const initDb = async () => {
+  try {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS  users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20),
+        email VARCHAR(20) UNIQUE NOT NULL,
+        password VARCHAR(20) NOT NULL,
+        is_active BOOLEAN DEFAULT true,
+        age INT,
+
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+
+        )
+        `);
+    console.log("Database connected successfully! ");
+  } catch (error) {
+    console.log(error);
+  }
+};
