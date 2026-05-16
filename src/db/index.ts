bcae45139ffd6c37fc957279f15cd6e5e998c4e2
Tomal Hossen <tomalhossen78@gmail.com>
2026-05-16 16:35:41 +0600

    managing user profile with SQL reference

--- src/db/index.ts
+++ src/db/index.ts
@@ -21,6 +21,23 @@ export const initDb = async () => {
 
         )
         `);
+
+    await pool.query(`
+            CREATE TABLE IF NOT EXISTS  profiles(
+            id SERIAL PRIMARY KEY,
+            user_id INT UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+
+            bio TEXT,
+            address TEXT,
+            phone VARCHAR(15),
+            gender VARCHAR(10), 
+            
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+
+            )
+
+            `);
     console.log("Database connected successfully! ");
   } catch (error) {
     console.log(error);
