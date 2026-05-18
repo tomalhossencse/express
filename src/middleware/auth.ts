55c32295d2e3488ea207dd461c3dc7e54cf0526e
Tomal Hossen <tomalhossen78@gmail.com>
2026-05-18 19:27:02 +0600

    add : auth middleware validation

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,16 +1,50 @@
+import jwt, { type JwtPayload } from "jsonwebtoken";
 import { type NextFunction, type Request, type Response } from "express";
+import config from "../config";
+import { pool } from "../db";
+import type { IUser } from "../modules/user/user.interface";
 
 const auth = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
     // console.log("This is protected Route");
     // console.log(req.headers.authorization);
     const token = req.headers.authorization;
+
     if (!token) {
       res.status(401).json({
         success: false,
         message: "Unauthorized access",
       });
     }
+
+    const decoded = jwt.verify(
+      token as string,
+      config.secret as string,
+    ) as JwtPayload;
+    console.log(decoded);
+
+    const userData = await pool.query(
+      `SELECT * FROM users
+        WHERE email = $1
+        `,
+      [decoded.email],
+    );
+    const user: IUser = userData.rows[0];
+
+    if (userData.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    if (!user.is_active) {
+      res.status(403).json({
+        success: false,
+        message: "Forbidden!",
+      });
+    }
+    console.log(user);
     next();
   };
 };
