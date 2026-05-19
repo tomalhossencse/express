fcc2f541aee9f167aa8edb2a14544294a2bb38d3
Tomal Hossen <tomalhossen78@gmail.com>
2026-05-19 15:16:43 +0600

    cors policy and global error handle

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import express, {
   type Request,
   type Response,
 } from "express";
+import cors from "cors";
 import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./modules/auth/auth.route";
 import logger from "./middleware/logger";
 import cookieParser from "cookie-parser";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app: Application = express();
 
@@ -16,6 +18,12 @@ app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  }),
+);
 
 app.get("/", (req: Request, res: Response) => {
   //   res.send("Hello World!");
@@ -28,4 +36,8 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/api/users", userRoute);
 app.use("/api/profiles", profileRoute);
 app.use("/api/auth", authRoute);
+
+// Global Error Handling Middleware
+app.use(globalErrorHandler);
+
 export default app;
